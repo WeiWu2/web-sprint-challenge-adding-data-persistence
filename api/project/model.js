4f76ec1624c9172480177a2bba1b8d7581374ed8
WeiWu2 <weidiwu207@gmail.com>
2021-03-19 15:38:50 -0400

    set up projects router

--- api/project/model.js
+++ api/project/model.js
@@ -1,14 +1,36 @@
-// build your `Project` model here
 const db = require('../../data/dbConfig')
 
 
-const get = () => {
-    return db('projects')
+const convertProjectBoolean = (project) => {
+    if(project.project_completed === 0)
+    return{...project, project_completed: false}
+    else
+    return {...project, project_completed: true}
 }
 
+const get = async () => {
+    const projects = await db('projects')
+    const newProject = projects.map((project) => {
+       return convertProjectBoolean(project)
+    })
+    return newProject
+}
 
+const findById = (id) => {
+    return db('projects')
+    .where('project_id', id).first()
+}
 
+const create = async (project) => {
+    const id = await db('projects')
+    .insert(project)
+   const newProject = await findById(id)
+   return convertProjectBoolean(newProject)
+}
 
 module.exports = {
-    get
+    get,
+    create,
+    findById,
+    convertProjectBoolean
 }
