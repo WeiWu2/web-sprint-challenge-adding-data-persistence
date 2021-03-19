dd1bb6b3fb04ce8b10a2b0d5d1cf0def3b3d2871
WeiWu2 <weidiwu207@gmail.com>
2021-03-19 15:56:13 -0400

    resource router set up

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,22 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+
+const getResources = () => {
+    return db('resources')
+}
+
+const findResourceById = (id) => {
+    return db('resources')
+    .where('resource_id', id).first()
+}
+const createResource = async (resource) => {
+    return findResourceById(await db('resources').insert(resource))
+}
+
+
+
+module.exports = {
+    getResources,
+    createResource,
+    findResourceById
+}
